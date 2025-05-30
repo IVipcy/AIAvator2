9fb6d56f372ba25d5d19ee0e9237c36cdaad7d4e
Suguru Fukushima <suguru.fukushima@congen-ai.com>
2025-05-30 15:14:38 +0900

    fix: Update Python version configuration and build script

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-# Install Python 3.9
-curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-pyenv install 3.9.18
-pyenv global 3.9.18
+# Set Python version
+export PYTHON_VERSION=3.9
+export VERCEL_PYTHON_RUNTIME=3.9
+
+# Install Python 3.9 using pyenv
+if [ ! -d "$HOME/.pyenv" ]; then
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+    pyenv install 3.9.18
+    pyenv global 3.9.18
+fi
+
+# Verify Python version
+python --version
 
 # Install dependencies
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt 
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt 
